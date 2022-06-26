e8425e215b4bdbff98f2796ba8c744b394619a8b
Yaseen <Yaseen98@hotmail.co.uk>
2022-06-26 17:04:24 +0100

    2. Floating sum view  - Added summary view

--- Project/TechChallenge/View/TransactionList/TransactionListView.swift
+++ Project/TechChallenge/View/TransactionList/TransactionListView.swift
@@ -11,6 +11,21 @@ struct TransactionListView: View {
     let transactions: [TransactionModel] = ModelData.sampleTransactions
     let categories: [TransactionModel.Category] = TransactionModel.Category.allCases
     @State var selectedCategory: TransactionModel.Category = .defaultCase
+    @State var selectedTotal: Double = setSelectedTotal()
+    
+    static func setSelectedTotal() -> Double {
+        let transactions = ModelData.sampleTransactions
+        let amounts = transactions.map{ $0.amount }
+        let total = amounts.reduce(0, +)
+        return total
+    }
+    
+    func updateSelectedTotal() {
+        let filteredTransactions = TransactionModel.filter(transactions: transactions, selectedCategory: selectedCategory)
+        let amounts = filteredTransactions.map{ $0.amount }
+        let total = amounts.reduce(0, +)
+        selectedTotal = total
+    }
     
     var body: some View {
         VStack {
@@ -19,6 +34,7 @@ struct TransactionListView: View {
                     ForEach(categories) { category in
                         CategoryButtonView(name: category.rawValue, colour: category.color, action: {
                             selectedCategory = category
+                            updateSelectedTotal()
                         })
                     }
                 }.padding()
@@ -31,6 +47,8 @@ struct TransactionListView: View {
             }
             .animation(.easeIn)
             .listStyle(PlainListStyle())
+            
+            SummaryView(selectedCategory: $selectedCategory, total: $selectedTotal)
         }
         .navigationBarTitleDisplayMode(.inline)
         .navigationTitle("Transactions")
@@ -61,5 +79,36 @@ struct CategoryButtonView: View {
                 .padding()
         }).background(colour)
             .clipShape(Capsule())
+    }
+}
+
+struct SummaryView: View {
+    @Binding var selectedCategory: TransactionModel.Category
+    @Binding var total: Double
+    
+    var body: some View {
+        VStack {
+            Text(selectedCategory.rawValue)
+                .font(.system(.headline))
+                .foregroundColor(selectedCategory.color)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            HStack {
+                Text("Total spent:")
+                    .foregroundColor(.secondary)
+                
+                Spacer()
+                
+                let readableTotal = String(format: "$%.02f", total)
+                Text(readableTotal)
+                    .bold()
+                    .foregroundColor(.secondary)
+            }
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.accentColor, lineWidth: 2)
+        ).padding(8)
     }
 }
